1919a4f49d1794238ddec69855fecfa5cc0f6528
Ismail Karaca <Ismailkaracayt@gmail.com>
2023-08-15 21:20:16 +0200

    Functions

--- MPT-Health-Project/main.py
+++ MPT-Health-Project/main.py
@@ -1,1 +1,35 @@
 import cv2
+
+
+def select_camera(camera):
+    pass
+
+
+# function, which calculates the skeleton of a frame, with a CNN (PoseNet)
+def detect_skeleton(frame):
+    pass
+
+
+# function, which shows the camera-input, with the skeleton
+def show_skeleton():
+    pass
+
+
+# function, which evaluates the skeleton and gives it a score
+def posture_score():
+    pass
+
+
+# function, which gives alerts to the user, when the posture_score is too low.
+def score_alerts():
+    pass
+
+
+# function, which saves the posture-Score + timestamp in a csv_file
+def posture_score_csv():
+    pass
+
+
+# function, which displays the csv_file as a dashboard. The Posture-Score of the day will be shown.
+def dashboard(file):
+    pass
