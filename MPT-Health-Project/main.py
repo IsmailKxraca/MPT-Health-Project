d851485e848136b917db7cb72a394c0d86f96b51
Ismail <ismailkaracayt@gmail.com>
2023-09-23 00:55:14 +0200

    pose estimation with Mediapipe

--- MPT-Health-Project/main.py
+++ MPT-Health-Project/main.py
@@ -1,31 +1,48 @@
 import cv2
+import mediapipe as mp
+
 
 # initialises the camera. (First Camera in System)
 cap = cv2.VideoCapture(0)
 
-while True:
-    # captures one frame of the camera
-    ret, frame = cap.read()
 
-    if not ret:
-        break
+# output = pose-estimation via mediapipe
+def pose_output():
+    # initialises the Pose-Estimation-Pipeline
+    mp_pose = mp.solutions.pose
+    pose = mp_pose.Pose()
 
-    # shows the camera-input
-    cv2.imshow("Kamera", frame)
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
 
-    # end the loop, when "q" is clicked
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        # transforming the frame to RGB
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        # performing of the pose-estimation on the frame
+        results = pose.process(frame_rgb)
+
+        # results.pose_landmarks = Position of the body nodes
+        if results.pose_landmarks:
+            # projects the nodes on the camera-output
+            mp_drawing = mp.solutions.drawing_utils
+            # projects the links between the nodes on the camera-output
+            mp_drawing.draw_landmarks(frame, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+
+        # projects
+        cv2.imshow("Pose Estimation", frame)
+
+        # loop ends when clicked on esc or q
+        key = cv2.waitKey(1)
+        if key == 27 or key == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
 
 
-# function, which calculates the skeleton of a frame, with a CNN (PoseNet)
-def detect_skeleton(frame):
-    pass
-
-
-# function, which shows the camera-input, with the skeleton
-def show_skeleton():
-    pass
+pose_output()
 
 
 # function, which evaluates the skeleton and gives it a score
